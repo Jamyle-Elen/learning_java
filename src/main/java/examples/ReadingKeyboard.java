81b3cd1552aa0f0b876f332825b3e0d020c16db8
Jamyle Elen <jamyleelen09@gmail.com>
2024-12-22 17:45:20 -0300

    teste git

--- src/main/java/examples/ReadingKeyboard.java
+++ src/main/java/examples/ReadingKeyboard.java
@@ -9,6 +9,8 @@ public class ReadingKeyboard {
         int number01 = teclado.nextInt();
         System.out.println(number01);
         System.out.println("Digite uma palavra: ");
+        String mensagem = teclado.next();
+        System.out.println(mensagem);
         System.out.println("Digite outro número: ");
         int number02 = teclado.nextInt();
         System.out.println("Soma de number01 + number02 vale: " + (number01 + number02));
