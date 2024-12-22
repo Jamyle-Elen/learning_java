18de1a34949162676a2b42219b0070e7258c3792
Jamyle Elen <Jamyle Elen>
2024-12-22 13:57:24 -0300

    tipos de variaveis

--- src/main/java/examples/ExamplesDataTypes.java
+++ src/main/java/examples/ExamplesDataTypes.java
@@ -1,7 +1,20 @@
 package examples;
 
 public class ExamplesDataTypes {
-    public static void main(String[] args) {
-        System.out.println("Oie!");
+    public static void main(String[] args){
+        int valor01;
+        float valor02;
+        boolean valor03;
+        char letra;
+        valor01 = 12;
+        valor02 = 10.2f;
+        valor03 = true;
+        letra = 'a';
+
+        System.out.println(valor02);
+        System.out.println("Valor das variavel 1: " + valor01);
+        System.out.println("Valor das variavel 2: " + valor02);
+        System.out.println("Valor das variavel 3: " + valor03);
+        System.out.println("Valor das variavel 3: " + letra);
     }
 }
